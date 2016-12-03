faf29c37e053e866f7ce2b115020ccc78b416cdc
Xylar Asay-Davis <xylarstorm@gmail.com>
2016-12-03 11:33:57 +0100

    Switch from getlist to getExpression in config parser
    This will support more general python data structures, such as
    dictionaries, tuples, etc.  It will also support lists of lists,
    which will be useful for later work mapping variable names as they
    change between MPAS versions.
    
    This changes the syntax supported for lists of strings: they must
    now be in single or double quotes.
    
    The function setlist has been removed, since lists and other data
    structures can be converted to strings with str(dataStructure).

--- mpas_analysis/configuration/MpasAnalysisConfigParser.py
+++ mpas_analysis/configuration/MpasAnalysisConfigParser.py
@@ -1,11 +1,11 @@
 import numbers
+import ast
 
 """
 A configuratin parser class for MPAS analysis.  MpasAnalysisConfigParser adds
 the capabilities to get an option including a default value
-(`getWithDefault(section, option, default, ...)`) and to get and set options
-that are lists (`getList(section, option, ...)` and
-`setList(secton, option, value)`).
+(`getWithDefault(section, option, default, ...)`) and to get options
+that are lists, tuples, dicts, etc (`getExpression(section, option)`).
 
 Author: Xylar Asay-Davis
 Last Modified: 12/03/2016
@@ -16,15 +16,13 @@ from ConfigParser import ConfigParser
 
 class MpasAnalysisConfigParser(ConfigParser):
 
-    def getWithDefault(self, section, option, default, listType=None):
+    def getWithDefault(self, section, option, default):
         """
         Get an option, using the supplied default value if the option is not
         present.  The type of `default` is used to determine the type of the
         retrun value, regardless of whether `default` is returned. If `default`
-        is a list and the given option is present, `listType` may be supplied
-        to determine the type of the resulting list.  If `listType` is not
-        supplied and `default` is a list, the default type of entries in the
-        resulting list is the type of the first item in `default`.
+        is a list, tuple, or dict, `getExpression(...)` is used if the option
+        is present in the config file.
 
         Author: Xylar Asay-Davis
         Last Modified: 12/03/2016
@@ -37,60 +35,24 @@ class MpasAnalysisConfigParser(ConfigParser):
                     return self.getfloat(section, option)
                 elif isinstance(default, bool):
                     return self.getboolean(section, option)
-                elif isinstance(default, list):
-                    if (listType is None) and (len(default) > 0):
-                        # use the type of default[0] for listType
-                        listType = type(default[0])
-
-                    return self.getlist(section, option, listType)
+                elif isinstance(default, (list, tuple, dict)):
+                    return self.getExpression(section, option)
                 else:
                     return self.get(section, option)
 
         # we didn't find the entry so set it to the default
-        if isinstance(default, list):
-            self.setlist(section, option, default)
-        else:
-            self.set(section, option, str(default))
+        self.set(section, option, str(default))
         return default
 
-    def getlist(self, section, option, listType=str):
+    def getExpression(self, section, option):
         """
-        Get an option as a list.  `section` and `option` work as in `get(...)`.
-        `listType` is the type of each entry in the resulting list (`str` by
-        default).  Quotes around strings in the list will be stripped (but
-        are not required).
+        Get an option as an expression (typically a list, though tuples and
+        dicts should also work).  `section` and `option` work as in `get(...)`.
+        The expression is required to have valid python syntax, so that
+        string entries are required to be in single or double quotes.
 
         Author: Xylar Asay-Davis
         Last Modified: 12/03/2016
         """
-        listString = self.get(section, option)
-        # first, strip the brackets
-        listString = listString.strip('[]')
-        if (listType == str) and ('"' in listString):
-            # assume all entries are strings separated by quotes,
-            # but every other one is the comma separating list entries
-            stringList = listString.split('"')[0::2]
-        elif (listType == str) and ("'" in listString):
-            # assume all entries are strings separated by quotes,
-            # but every other one is the comma separating list entries
-            stringList = listString.split("'")[0::2]
-        else:
-            stringList = listString.split(',')
-        outList = []
-        for string in stringList:
-            outList.append(listType(string.strip()))
-        return outList
-
-    def setlist(self, section, option, value):
-        """
-        Sets an option to be a list based on entries in `value`, which must
-        be iterable.  Entries are converted to strings with `str`, so
-        it may be desirable to convert floating-point values to stirngs before
-        calling this routine if more constrol over the precision is required.
-
-        Author: Xylar Asay-Davis
-        Last Modified: 12/03/2016
-        """
-        listString = ', '.join(str(entry) for entry in value)
-        listString = '[{}]'.format(listString)
-        self.set(section, option, listString)
+        expressionString = self.get(section, option)
+        return ast.literal_eval(expressionString)
