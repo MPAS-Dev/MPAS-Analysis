21626044b340f45008f13c3d639827e67addc573
Xylar Asay-Davis <xylarstorm@gmail.com>
2016-12-04 23:15:36 +0100

    Add ocean dicts mapping stream and variable names
    This merge adds two dictionaries that are used to map variable
    and stream names from various MPAS-O versions to the corresponding
    values in MPAS-Analysis.

---
+++ mpas_analysis/ocean/variable_stream_map.py
@@ -0,0 +1,46 @@
+# mappings of stream names from various MPAS-O versions to those in
+# mpas_analysis
+oceanStreamMap = {'timeSeriesStats': ['timeSeriesStatsOutput',
+                                      'timeSeriesStatsMonthly',
+                                      'timeSeriesStatsMonthlyOutput']}
+
+
+oceanVariableMap = {}
+oceanVariableMap['Time'] = [['xtime_startMonthly', 'xtime_endMonthly'],
+                            ['xtime_start', 'xtime_end'],
+                            'time_avg_daysSinceStartOfSim']
+
+# SST
+oceanVariableMap['avgSurfaceTemperature'] = \
+    ['time_avg_avgValueWithinOceanRegion_avgSurfaceTemperature',
+     'time_avg_avgValueWithinOceanRegion_avgSurfaceTemperature_1',
+     'timeMonthly_avg_avgValueWithinOceanRegion_avgSurfaceTemperature']
+
+# OHC
+oceanVariableMap['avgLayerTemperature'] = \
+    ['time_avg_avgValueWithinOceanLayerRegion_avgLayerTemperature',
+     'time_avg_avgValueWithinOceanLayerRegion_avgLayerTemperature_1',
+    'timeMonthly_avg_avgValueWithinOceanLayerRegion_avgLayerTemperature']
+oceanVariableMap['sumLayerMaskValue'] = \
+    ['time_avg_avgValueWithinOceanLayerRegion_sumLayerMaskValue',
+     'time_avg_avgValueWithinOceanLayerRegion_sumLayerMaskValue_1',
+     'timeMonthly_avg_avgValueWithinOceanLayerRegion_sumLayerMaskValue']
+oceanVariableMap['avgLayerArea'] = \
+    ['time_avg_avgValueWithinOceanLayerRegion_avgLayerArea',
+     'time_avg_avgValueWithinOceanLayerRegion_avgLayerArea_1',
+     'timeMonthly_avg_avgValueWithinOceanLayerRegion_avgLayerArea']
+oceanVariableMap['avgLayerThickness'] = \
+    ['time_avg_avgValueWithinOceanLayerRegion_avgLayerThickness',
+     'time_avg_avgValueWithinOceanLayerRegion_avgLayerThickness_1',
+     'timeMonthly_avg_avgValueWithinOceanLayerRegion_avgLayerThickness']
+
+# model vs. obs.
+oceanVariableMap['mld'] = \
+    ['time_avg_dThreshMLD',
+     'time_avg_dThreshMLD_1',
+     'timeMonthly_avg_dThreshMLD']
+
+oceanVariableMap['sst'] = \
+    ['time_avg_activeTracers_temperature',
+     'time_avg_activeTracers_temperature_1',
+     'timeMonthly_avg_activeTracers_temperature']
