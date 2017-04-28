d50c404965ceed6dcd0846664104d677f7e5dc7e
Xylar Asay-Davis <xylarstorm@gmail.com>
2017-04-28 21:41:14 +0200

    Switch ocean climatology map tasks to be classes
    They decend from a common class (OceanClimatologyMap) that decends
    from AnalysisTask.
    
    Update run_analysis.py to use analysis classes. Get task, component and
    category names from analysis tasks. Call 'check' functions to make sure
    config is correct and files are present. Later, call 'run' on each analysis
    task.
    
    Move check_generate to AnalysisTask
    
    Update the config file to reflect the rename from regridded(Horizontal)
    to climatologyMap

--- mpas_analysis/ocean/__init__.py
+++ mpas_analysis/ocean/__init__.py
@@ -1,5 +1,6 @@
-from ocean_modelvsobs import ocn_modelvsobs
-from nino34_index import nino34_index
-from sst_timeseries import sst_timeseries
-from ohc_timeseries import ohc_timeseries
-from meridional_overturning_circulation import moc_streamfunction
+from climatology_map import ClimatologyMapSST, ClimatologyMapMLD, \
+    ClimatologyMapSSS
+# from nino34_index import nino34_index
+# from sst_timeseries import sst_timeseries
+# from ohc_timeseries import ohc_timeseries
+# from meridional_overturning_circulation import moc_streamfunction
