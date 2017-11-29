16a31fe7197c22ce9c39fd3563f9ffce57f45dd0
Xylar Asay-Davis <xylarstorm@gmail.com>
2017-11-29 12:08:33 +0100

    Fix integer division in IndexNino34

--- mpas_analysis/ocean/index_nino34.py
+++ mpas_analysis/ocean/index_nino34.py
@@ -397,7 +397,7 @@ class IndexNino34(AnalysisTask):  # {{{
         """
 
         nt = len(inputData)
-        sp = (len(wgts) - 1)/2
+        sp = (len(wgts) - 1) // 2
         runningMean = inputData.copy()
         for k in range(sp, nt-(sp+1)):
             runningMean[k] = sum(wgts*inputData[k-sp:k+sp+1].values)
