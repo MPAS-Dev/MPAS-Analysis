93879d39440b43e9cfe76fb9fcd4c881e52b4318
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-01-02 20:38:33 +0100

    Add util funs for anomalies and moving avgs
    These new functions compute the moving average of a data set over
    a window of samples in time, and the anomaly of that moving average
    from the beginning of an MPAS simulation.

--- mpas_analysis/shared/time_series/__init__.py
+++ mpas_analysis/shared/time_series/__init__.py
@@ -1,2 +1,6 @@
 from .time_series import cache_time_series
 from .mpas_time_series_task import MpasTimeSeriesTask
+
+from .anomaly import compute_moving_avg_anomaly_from_start
+from .moving_average import compute_moving_avg
+
