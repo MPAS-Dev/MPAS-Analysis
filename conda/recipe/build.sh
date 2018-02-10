b506b8c23ad9ed3a5c1d0abdc2f6be277f28c4cf
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-02-10 11:04:16 -0700

    Add license headers to source files

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/sh
+# Copyright (c) 2017,  Los Alamos National Security, LLC (LANS)
+# and the University Corporation for Atmospheric Research (UCAR).
+#
+# Unless noted otherwise source code is licensed under the BSD license.
+# Additional copyright and license information can be found in the LICENSE file
+# distributed with this code, or at http://mpas-dev.github.com/license.html
+#
 python setup.py install --single-version-externally-managed --record=record.txt
--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# Copyright (c) 2017,  Los Alamos National Security, LLC (LANS)
+# and the University Corporation for Atmospheric Research (UCAR).
+#
+# Unless noted otherwise source code is licensed under the BSD license.
+# Additional copyright and license information can be found in the LICENSE file
+# distributed with this code, or at http://mpas-dev.github.com/license.html
+#
 
 from setuptools import setup, find_packages
 import warnings
