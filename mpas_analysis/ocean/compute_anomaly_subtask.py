6a0ea795f6ab3d2e221e4a0cba2bcbc48d5a8501
Luke Van Roekel <luke.vanroekel@gmail.com>
2018-03-19 13:04:24 -0500

    Fixes for ref start year
    Adds anomalyEndTime to open_mpas_dataset for reference end.

--- mpas_analysis/ocean/compute_anomaly_subtask.py
+++ mpas_analysis/ocean/compute_anomaly_subtask.py
@@ -148,13 +148,17 @@ class ComputeAnomalySubtask(AnalysisTask):
         if config.has_option('timeSeries', 'anomalyRefYear'):
             anomalyYear = config.getint('timeSeries', 'anomalyRefYear')
             anomalyRefDate = '{:04d}-01-01_00:00:00'.format(anomalyYear)
+            anomalyEndDate = '{:04d}-12-31_23:59:59'.format(anomalyYear)
         else:
             anomalyRefDate = get_simulation_start_time(self.runStreams)
+            anomalyYear = int(anomalyRefDate[0:4])
+            anomalyEndDate = '{:04d}-12-31_23:59:59'.format(anomalyYear)
 
         ds = compute_moving_avg_anomaly_from_start(
                 timeSeriesFileName=self.inputFile,
                 variableList=self.variableList,
-                simulationStartTime=anomalyRefDate,
+                anomalyStartTime=anomalyRefDate,
+                anomalyEndTime=anomalyEndDate,
                 startDate=startDate,
                 endDate=endDate,
                 calendar=self.calendar,
