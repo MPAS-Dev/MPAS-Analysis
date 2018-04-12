b5f50898a0b7732364ce2272eb2616095fecd205
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-04-12 10:35:32 +0200

    Move nan to numpy mask funciton to utility
    This funciton will be used by multiple tasks.
    
    Also, remove an attribute from a docstring that is no longer
    present.

--- mpas_analysis/ocean/utility.py
+++ mpas_analysis/ocean/utility.py
@@ -63,4 +63,13 @@ def compute_zmid(bottomDepth, maxLevelCell, layerThickness):  # {{{
 
     return zMid  # }}}
 
+
+def nans_to_numpy_mask(field):  # {{{
+    """
+    Convert a numpy array with NaNs to a masked numpy array
+    """
+    field = numpy.ma.masked_array(
+        field, numpy.isnan(field))
+    return field  # }}}
+
 # vim: foldmethod=marker ai ts=4 sts=4 et sw=4 ft=python
