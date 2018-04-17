55650c770bfc8adf3f8e9ba7379f34de0903aa84
Phillip J. Wolfram <phillipwolfram@gmail.com>
2018-04-17 10:58:15 -0600

    PEP8 cleanup

--- docs/parse_table.py
+++ docs/parse_table.py
@@ -12,16 +12,18 @@ import tabulate
 import argparse
 import re
 
+
 def markdown_links(data, footer):
     urlscmd = re.findall(r"\[.*?\]\n*\(.*?\)", data)
     urls = re.findall(r"\[.*?\]\n*\((.*?)\)", data)
     linknames = re.findall(r"\[(.*?)\]\n*\(.*?\)", data)
 
     for alinkname, aurl, aurlscmd in zip(linknames, urls, urlscmd):
-        data = data.replace(aurlscmd, '`' + alinkname +'`_')
+        data = data.replace(aurlscmd, '`' + alinkname + '`_')
         footer += ".. _`" + alinkname + "`: " + aurl + "\n"
 
     return data, footer
+
 
 def spurious_newline_whitespace(data, _):
     whitespace = re.findall('\n\s*', data)
@@ -30,11 +32,13 @@ def spurious_newline_whitespace(data, _):
         data = data.replace(astr, "\n")
     return data, _
 
+
 def cleanup(linedata, footer):
     cleanups = [spurious_newline_whitespace, markdown_links]
     for acleanup in cleanups:
         linedata, footer = acleanup(linedata, footer)
     return linedata, footer
+
 
 def build_rst_table_from_xml(xmlfile, rstfile, component):
 
@@ -45,12 +49,14 @@ def build_rst_table_from_xml(xmlfile, rstfile, component):
     rst = open(rstfile, 'w')
 
     # get headers and build list to write entries
-    headers = xml.getroot().attrib['headers'].replace(' ','').split(',')
-    headernames = [aname.strip() for aname in xml.getroot().attrib['headernames'].split(',')]
+    headers = xml.getroot().attrib['headers'].replace(' ', '').split(',')
+    headernames = [aname.strip() for aname in
+                   xml.getroot().attrib['headernames'].split(',')]
     data = []
     footer = '\n'
     for entry in xml.findall('observation'):
-        if component != 'all' and entry.findall('component')[0].text.strip() != component:
+        if (component != 'all' and
+                entry.findall('component')[0].text.strip() != component):
             continue
         line = []
         for aheader in headers:
@@ -64,18 +70,22 @@ def build_rst_table_from_xml(xmlfile, rstfile, component):
 
     rst.close()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
             description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-x", "--xml_table", dest="xml_table",
-                        help="Path to file containing xml description of table",
+                        help="Path to file containing "
+                             "xml description of table",
                         metavar="FILE", required=True)
     parser.add_argument("-r", "--rst_table", dest="rst_table",
-                        help="Path to file containing rst description of table for output",
+                        help="Path to file containing rst description "
+                             "of table for output",
                         metavar="FILE", required=True)
     parser.add_argument("-c", "--component", dest="component",
-                        help="Component for parsing of table, 'landice', 'ocean', "
-                             "'seaice', or 'all'", metavar="STRING", default="all")
+                        help="Component for parsing of table: "
+                             "'landice', 'ocean', 'seaice', or 'all'",
+                        metavar="STRING", default="all")
 
     args = parser.parse_args()
 
