3ea5bd22060ea65f33ea8d37439ca8d2b5b5c341
“Xylar <xylarstorm@gmail.com>
2018-05-17 21:14:59 +0200

    Update scripts to use common download_files func.

--- download_analysis_data.py
+++ download_analysis_data.py
@@ -25,80 +25,8 @@ from __future__ import absolute_import, division, print_function, \
 import os
 import argparse
 import pkg_resources
-import requests
-import progressbar
 
-
-# From https://stackoverflow.com/a/1094933/7728169
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
-
-
-def download_analysis_files(outDir):
-    urlBase = 'https://web.lcrc.anl.gov/public/e3sm/diagnostics'
-    analysisFileList = pkg_resources.resource_string(
-            'mpas_analysis', 'obs/analysis_input_files').decode('utf-8')
-
-    for fileName in analysisFileList.split('\n'):
-        outFileName = '{}/{}'.format(outDir, fileName)
-        # outFileName contains full path, so we need to make the relevant
-        # subdirectories if they do not exist already
-        directory = os.path.dirname(outFileName)
-        try:
-            os.makedirs(directory)
-        except OSError:
-            pass
-
-        url = '{}/{}'.format(urlBase, fileName)
-        try:
-            response = requests.get(url, stream=True)
-            totalSize = response.headers.get('content-length')
-        except requests.exceptions.RequestException:
-            print('  {} could not be reached!'.format(fileName))
-            continue
-
-        if totalSize is None:
-            # no content length header
-            if not os.path.exists(outFileName):
-                with open(outFileName, 'wb') as f:
-                    print('Downloading {}...'.format(fileName))
-                    try:
-                        f.write(response.content)
-                    except requests.exceptions.RequestException:
-                        print('  {} failed!'.format(fileName))
-                    else:
-                        print('  {} done.'.format(fileName))
-        else:
-            # we can do the download in chunks and use a progress bar, yay!
-
-            totalSize = int(totalSize)
-            if os.path.exists(outFileName) and \
-                    totalSize == os.path.getsize(outFileName):
-                # we already have the file, so just continue
-                continue
-
-            print('Downloading {} ({})...'.format(fileName,
-                                                  sizeof_fmt(totalSize)))
-            widgets = [progressbar.Percentage(), ' ', progressbar.Bar(),
-                       ' ', progressbar.ETA()]
-            bar = progressbar.ProgressBar(widgets=widgets,
-                                          maxval=totalSize).start()
-            size = 0
-            with open(outFileName, 'wb') as f:
-                try:
-                    for data in response.iter_content(chunk_size=4096):
-                        size += len(data)
-                        f.write(data)
-                        bar.update(size)
-                    bar.finish()
-                except requests.exceptions.RequestException:
-                    print('  {} failed!'.format(fileName))
-                else:
-                    print('  {} done.'.format(fileName))
+from mpas_analysis.shared.io.download import download_files
 
 
 if __name__ == "__main__":
@@ -115,4 +43,10 @@ if __name__ == "__main__":
     except OSError:
         pass
 
-    download_analysis_files(args.outDir)
+    urlBase = 'https://web.lcrc.anl.gov/public/e3sm/diagnostics'
+    analysisFileList = pkg_resources.resource_string(
+            'mpas_analysis', 'obs/analysis_input_files').decode('utf-8')
+
+    # remove any empty strings from the list
+    analysisFileList = list(filter(None, analysisFileList.split('\n')))
+    download_files(analysisFileList, urlBase, args.outDir)
