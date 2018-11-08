f9602f321e5033fb4358cdce44b9c91f1d627d56
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-11-07 22:10:49 -0500

    Move run_mpas_analysis to entry point
    To call run the analysis, now do:
    python -m mpas_analysis ...
    
    Once built, the package can be run with:
    mpas_analysis ...
    
    download_analysis_datay.py now points to a function in __main__.py

--- download_analysis_data.py
+++ download_analysis_data.py
@@ -10,46 +10,13 @@
 # distributed with this code, or at
 # https://raw.githubusercontent.com/MPAS-Dev/MPAS-Analysis/master/LICENSE
 
-"""
-A script for downloading the input data set from public repository for
-MPAS-Analysis to work. The input data set includes: pre-processed
-observations data, MPAS mapping files and MPAS regional mask files
-(which are used for the MOC computation), for a subset of MPAS meshes.
-"""
-# Authors
-# -------
-# Milena Veneziani
-# Xylar Asay-Davis
-
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 
-import os
-import argparse
-import pkg_resources
-
-from mpas_analysis.shared.io.download import download_files
+from mpas_analysis.__main__ import download_analysis_data
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-o", "--outDir", dest="outDir", required=True,
-                        help="Directory where MPAS-Analysis input data will"
-                             "be downloaded")
-    args = parser.parse_args()
-
-    try:
-        os.makedirs(args.outDir)
-    except OSError:
-        pass
-
-    urlBase = 'https://web.lcrc.anl.gov/public/e3sm/diagnostics'
-    analysisFileList = pkg_resources.resource_string(
-            'mpas_analysis', 'obs/analysis_input_files').decode('utf-8')
-
-    # remove any empty strings from the list
-    analysisFileList = list(filter(None, analysisFileList.split('\n')))
-    download_files(analysisFileList, urlBase, args.outDir)
+    download_analysis_data()
--- run_mpas_analysis.py
+++
@@ -1,1 +0,0 @@
-run_mpas_analysis
