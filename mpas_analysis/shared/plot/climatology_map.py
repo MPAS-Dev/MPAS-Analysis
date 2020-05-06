d3176523a98cb934665e84da045a07577c3c732f
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-05-06 22:08:43 +0200

    Change ice shelves to be light gray

--- mpas_analysis/shared/plot/climatology_map.py
+++ mpas_analysis/shared/plot/climatology_map.py
@@ -580,7 +580,7 @@ def _add_land_lakes_coastline(ax):
             facecolor='gray', linewidth=0.5)
     ice_50m = cartopy.feature.NaturalEarthFeature(
             'physical', 'antarctic_ice_shelves_polys', '50m', edgecolor='k',
-            facecolor='gray', linewidth=0.5)
+            facecolor='lightgray', linewidth=0.5)
     lakes_50m = cartopy.feature.NaturalEarthFeature(
             'physical', 'lakes', '50m', edgecolor='k',
             facecolor='white',
