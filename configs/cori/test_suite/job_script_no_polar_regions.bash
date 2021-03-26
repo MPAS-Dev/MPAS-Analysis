3276be3331f21313c28ca71116b86afbce8e0453
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-03-26 03:00:43 -0700

    Add cori test suite

---
+++ configs/cori/test_suite/job_script_no_polar_regions.bash
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --partition=regular
+#SBATCH -C haswell
+#SBATCH --nodes=1
+#SBATCH --time=2:00:00
+#SBATCH --account=e3sm
+#SBATCH --job-name=mpas_analysis
+#SBATCH --output=mpas_analysis.o%j
+#SBATCH --error=mpas_analysis.e%j
+#SBATCH -L cscratch1,SCRATCH,project
+
+cd $SLURM_SUBMIT_DIR
+export OMP_NUM_THREADS=1
+
+source ${HOME}/miniconda3/etc/profile.d/conda.sh
+conda activate test_env
+export HDF5_USE_FILE_LOCKING=FALSE
+
+echo env: test_env
+echo configs: ../no_polar_regions.cfg
+
+srun -N 1 -n 1 python -m mpas_analysis ../no_polar_regions.cfg --verbose
+
