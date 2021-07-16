839ccf18acf74951481e6bef545f44c484853414
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-07-16 09:50:08 +0200

    Update E3SM-Unified locations in example job scripts

--- configs/lcrc/job_script.anvil.bash
+++ configs/lcrc/job_script.anvil.bash
@@ -10,8 +10,7 @@
 cd $SLURM_SUBMIT_DIR
 export OMP_NUM_THREADS=1
 
-source /lcrc/soft/climate/e3sm-unified/load_latest_e3sm_unified.sh
-export HDF5_USE_FILE_LOCKING=FALSE
+source /lcrc/soft/climate/e3sm-unified/load_latest_e3sm_unified_anvil.sh
 
 srun -N 1 -n 1 mpas_analysis configs/polarRegions.conf 20201025.GMPAS-IAF.T62_oQU240wLI.anvil.cfg
 
