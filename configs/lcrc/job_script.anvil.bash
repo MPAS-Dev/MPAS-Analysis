d0df0d28844b282dc687ada08663d1e53b78f5bc
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-11-25 10:48:19 +0100

    Update LCRC example config and job scripts

--- configs/lcrc/job_script.anvil.bash
+++ configs/lcrc/job_script.anvil.bash
@@ -7,10 +7,17 @@
 #SBATCH --output=mpas_analysis.o%j
 #SBATCH --error=mpas_analysis.e%j
 
-cd $SLURM_SUBMIT_DIR
 export OMP_NUM_THREADS=1
 
 source /lcrc/soft/climate/e3sm-unified/load_latest_e3sm_unified_anvil.sh
+# alternatively, you can load your own development environment
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate mpas_dev
+# export E3SMU_MACHINE=anvil
 
-srun -N 1 -n 1 mpas_analysis configs/polarRegions.conf 20201025.GMPAS-IAF.T62_oQU240wLI.anvil.cfg
+export HDF5_USE_FILE_LOCKING=FALSE
+
+# For an E3SM cryosphere run, include --polar_regions, or exclude
+# this extra flag for default parameters
+mpas_analysis --polar_regions 20201025.GMPAS-IAF.T62_oQU240wLI.anvil.cfg
 
