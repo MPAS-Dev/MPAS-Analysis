7d5075fd3d01c8d4833b8dba556a96299a08de0e
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-11-25 10:32:28 +0100

    Update ALCF example configs and jobs script

---
+++ configs/alcf/job_script.cooley.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#COBALT -t 3:00:00
+#COBALT -n 1
+#COBALT -A ClimateEnergy_4
+
+source /lus/theta-fs0/projects/ccsm/acme/tools/e3sm-unified/load_latest_e3sm_unified_cooley.sh
+# alternatively, you can load your own development environment
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate mpas_dev
+# export E3SMU_MACHINE=cooley
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+# For an E3SM cryosphere run, include --polar_regions, or exclude
+# this extra flag for default parameters
+mpas_analysis 20180410.A_WCYCL1950_HR.ne120_oRRS18v3_ICG.theta.cfg
+
