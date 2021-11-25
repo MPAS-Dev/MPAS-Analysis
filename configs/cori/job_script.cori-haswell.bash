3d1e57f94bed2a1dd11997c9bb61b4eb3364adc3
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-11-25 10:12:38 +0100

    Update cori example job scripts and config

--- configs/cori/job_script.cori-haswell.bash
+++ configs/cori/job_script.cori-haswell.bash
@@ -1,15 +1,4 @@
 #!/bin/bash -l
-# This software is open source software available under the BSD-3 license.
-#
-# Copyright (c) 2020 Triad National Security, LLC. All rights reserved.
-# Copyright (c) 2020 Lawrence Livermore National Security, LLC. All rights
-# reserved.
-# Copyright (c) 2020 UT-Battelle, LLC. All rights reserved.
-#
-# Additional copyright and license information can be found in the LICENSE file
-# distributed with this code, or at
-# https://raw.githubusercontent.com/MPAS-Dev/MPAS-Analysis/master/LICENSE
-
 # comment out if using debug queue
 #SBATCH --partition=regular
 # comment in to get premium queue
@@ -26,22 +15,17 @@
 #SBATCH --error=mpas_analysis.e%j
 #SBATCH -L cscratch1,SCRATCH,project
 
-cd $SLURM_SUBMIT_DIR   # optional, since this is the default behavior
-
 export OMP_NUM_THREADS=1
 
 source /global/common/software/e3sm/anaconda_envs/load_latest_e3sm_unified_cori-haswell.sh
+# alternatively, you can load your own development environment
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate mpas_dev
+# export E3SMU_MACHINE=cori-haswell
 
-# MPAS/ACME job to be analyzed, including paths to simulation data and
-# observations. Change this name and path as needed
-run_config_file="config.run_name_here"
+export HDF5_USE_FILE_LOCKING=FALSE
 
-if [ ! -f $run_config_file ]; then
-    echo "File $run_config_file not found!"
-    exit 1
-fi
-
-# For an E3SM cryosphere run, include configs/polarRegions.conf, or exclude
-# this extra config file for default parameters
-srun -N 1 -n 1 mpas_analysis configs/polarRegions.conf $run_config_file
+# For an E3SM cryosphere run, include --polar_regions, or exclude
+# this extra flag for default parameters
+mpas_analysis --polar_regions run_name_here.cfg
 
