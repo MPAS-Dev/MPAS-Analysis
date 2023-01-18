31162042906ff2b3d969ffc8eeb5be0dfabc2567
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-01-18 15:45:36 -0600

    Fix depth slicing with non-ocean cells
    Previously, when there were cells with `maxLevelCell = 0`, this
    caused a failure because there was no valid depth coordinate
    for that cell and therefore no closest depth index could be found.
    
    This fix simply skips cells that are not in the ocean and masks
    them out.

--- mpas_analysis/ocean/remap_depth_slices_subtask.py
+++ mpas_analysis/ocean/remap_depth_slices_subtask.py
@@ -116,6 +116,7 @@ class RemapDepthSlicesSubtask(RemapMpasClimatologySubtask):
 
         zMid = compute_zmid(ds.bottomDepth, ds.maxLevelCell-1,
                             ds.layerThickness)
+        ocean_mask = (ds.maxLevelCell > 0)
 
         nVertLevels = zMid.shape[1]
         zMid.coords['verticalIndex'] = \
@@ -144,10 +145,12 @@ class RemapDepthSlicesSubtask(RemapMpasClimatologySubtask):
                 verticalIndices[depthIndex, :] = self.maxLevelCell.values
                 mask[depthIndex, :] = self.maxLevelCell.values >= 0
             else:
+                
+                diff = np.abs(zMid - depth).where(ocean_mask, drop=True)
+                verticalIndex = diff.argmin(dim='nVertLevels')
 
-                verticalIndex = np.abs(zMid - depth).argmin(dim='nVertLevels')
-
-                verticalIndices[depthIndex, :] = verticalIndex.values
+                verticalIndices[depthIndex, ocean_mask.values] = \
+                    verticalIndex.values
                 mask[depthIndex, :] = np.logical_and(depth <= zTop,
                                                      depth >= zBot).values
 
