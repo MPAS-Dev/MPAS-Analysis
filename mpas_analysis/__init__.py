ebc6fd8463ce21a1c414e3c1ea6dcbbb3f8332fb
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-08-03 11:50:16 +0200

    Update to v1.9.0rc4

--- mpas_analysis/__init__.py
+++ mpas_analysis/__init__.py
@@ -4,4 +4,5 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 __version_info__ = (1, 9, 0)
-__version__ = '.'.join(str(vi) for vi in __version_info__)
+__version__ = '1.9.0rc4'
+#__version__ = '.'.join(str(vi) for vi in __version_info__)
