e54dd45ca96d8331924d31c036b52e69cdfd7f7e
irenavankova <irenavankova@users.noreply.github.com>
2025-08-25 14:26:51 -0600

    Update mpas_analysis/ocean/time_series_antarctic_melt.py
    Co-authored-by: Xylar Asay-Davis <xylarstorm@gmail.com>

--- mpas_analysis/ocean/time_series_antarctic_melt.py
+++ mpas_analysis/ocean/time_series_antarctic_melt.py
@@ -789,7 +789,6 @@ class PlotMeltSubtask(AnalysisTask):
         # and cartopy doesn't play too well with tight_layout anyway
         plt.tight_layout()
 
-        #add_inset(fig, fc, width=2.0, height=2.0)
         add_inset(fig, fc, width=1.0, height=1.0, lowerleft=[0.0, 0.0], xbuffer=0.01, ybuffer=0.01)
 
         savefig(outFileName, config)
