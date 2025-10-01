0ad32598941342bb148eb79316d5101bf0aef936
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-10-01 09:12:19 -0500

    Update to version 1.14.0rc2

--- mpas_analysis/version.py
+++ mpas_analysis/version.py
@@ -1,2 +1,3 @@
-__version_info__ = (1, 13, 0)
-__version__ = '.'.join(str(vi) for vi in __version_info__)
+__version_info__ = (1, 14, 0)
+#__version__ = '.'.join(str(vi) for vi in __version_info__)
+__version__ = '1.14.0rc2'
